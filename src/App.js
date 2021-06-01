40da3a624a7dc250a2e3e0b93bf46873a1587905
AndreasCampan <andreascampan@gmail.com>
2021-05-31 22:12:31 -0600

    Final task patch
    -Added the mock data as a prop for eventlist

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberofEvents from './NumberofEvents';
+import { mockData } from './mock-data';
 
 function App() {
+  let events = mockData
   return (
     <div className="App">
-      <EventList />
+      <EventList events={events}/>
       <CitySearch />
       <NumberofEvents />
     </div>
