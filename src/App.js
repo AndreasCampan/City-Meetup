0d3abe8a75e7cfa43945ac62a5250469f6eda201
AndreasCampan <andreascampan@gmail.com>
2021-06-14 12:00:15 -0600

    Reverted Router and CSS Facelift
    -Reverted changes for Routing and css facelift and created an alternate branch for them

--- src/App.js
+++ src/App.js
@@ -1,40 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
-
-import LoginView from './LoginView';
-import HomeView from './HomeView';
-import NavView from './NavView';
-
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberofEvents from './NumberofEvents';
+import { extractLocations, getEvents, numFilter } from './api';
 
 class App extends Component {
-  state = {
-    user: false
+  constructor() {
+    super();
+    this.state = {
+      events: [],
+      locations: [],
+      eventsToShow: 32,
+      eventsLocFilt: [],
+      numFilteredList: []
+    }
+  }
+
+  componentDidMount() {
+    this.mounted= true;
+    getEvents()
+    .then((events) => {
+      if (this.mounted) {
+        this.setState({ 
+          events: events,
+          numFilteredList: events.slice(0, 32), 
+          locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    const locationEvents = (location === 'all') ?
+      this.state.events :
+      this.state.events.filter((event) => event.location === location);
+    this.setState({ 
+      eventsLocFilt: locationEvents,
+      numFilteredList: numFilter(locationEvents, this.state.eventsToShow)});
+  }
+
+  updateEventNum = (num) => {
+    if(this.state.eventsLocFilt.length !== 0){
+      this.setState({
+        eventsToShow: num,
+        numFilteredList: numFilter(this.state.eventsLocFilt, num)
+      });
+    } else {
+      this.setState({
+        eventsToShow: num,
+        numFilteredList: numFilter(this.state.events, num)
+      });
+    }
   }
 
   render() {
 
     return (
-      <Router>
-        <Route exact path="/City-Meetup" render={() => {
-          return (
-            <>
-              <NavView />
-              <HomeView />
-            </>
-          )
-        }} />
-
-        <Route exact path="/City-Meetup/Events" render={() => {
-          return (
-            <>
-              <NavView />
-              <LoginView />
-            </>
-          )
-        }} />
-      </Router>
+      <div className="content">
+        <div className="filters">
+          <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+          <NumberofEvents eventsToShow={this.state.eventsToShow} updateEventNum={this.updateEventNum}/>
+        </div>
+        <div className="eventbox">
+          <EventList events={this.state.numFilteredList}/>
+        </div>
+      </div>
     );
   }
 }
