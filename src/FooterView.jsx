8236c018a3e25c25e4abe41a53d18311d98025ce
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-08-15 19:58:24 -0600

    Update FooterView.jsx

--- src/FooterView.jsx
+++ src/FooterView.jsx
@@ -11,10 +11,10 @@ class FooterView extends Component {
     return (
       <footer className="footer">
         <div className="logos">
-          <a href="https://andreascampan.github.io/AC-Portfolio/" rel="noreferrer" alt="Link leading to the Creators Portfolio website" target="_blank">
+          <a href="https://www.andreascampan.com/" rel="noreferrer" alt="Link leading to the Creators Portfolio website" target="_blank">
             <img src={logo1} alt="Logo for Portfolio websites" className="logo1" />
           </a>
-          <a href="https://github.com/AndreasCampan" rel="noreferrer" alt="Link leading to this websites repository on github" target="_blank">
+          <a href="https://github.com/AndreasCampan/City-Meetup" rel="noreferrer" alt="Link leading to this websites repository on github" target="_blank">
             <img src={logo2} alt="Logo for github " className="logo2" />
           </a>
         </div>
